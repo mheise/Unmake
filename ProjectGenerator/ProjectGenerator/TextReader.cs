1700bb015bea25fddcff665d65c5b98fc499e1ec
kotarf <kotarf@rpi.edu>
2011-10-17 15:33:02 -0400

    Added assembly references to VCProjectEngine Working on base classes

---
+++ ProjectGenerator/ProjectGenerator/TextReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace ProjectGenerator
+{
+    class TextReader
+    {
+        public void read(string absfilepath)
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(absfilepath))
+                {
+                    String line;
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
