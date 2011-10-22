29f94ffecbd6c7328ac854263cb40911b5c4b47e
Mike Heise <michael.b.heise@gmail.com>
2011-10-22 02:48:17 -0400

    Added sample makefile to test, and made the parse print phony targets as a demo

--- parser.pl
+++ parser.pl
@@ -1,8 +1,13 @@
 #!/usr/bin/env perl
 use warnings;
 use strict;
+use 5.010;
 
 use Makefile::Parser::GmakeDB;
 
-my $db = `make --print-data-base -pqRrs -f Makefile`;
+my $db_listing = `make --print-data-base -pqRrs -f Makefile`;
+my $ast = Makefile::Parser::GmakeDB->parse(\$db_listing);
 
+# print phony targets
+my @phonies = ( keys %{$ast->phony_targets});
+say for @phonies;
