01093732bdcc5ea8d7f6d5a57e4291afaf51b271
Mike Heise <kaesees@gmail.com>
2012-02-19 02:06:30 -0500

    better function name, and more useful semantics

--- analyzer.lisp
+++ analyzer.lisp
@@ -1,7 +1,7 @@
 (ql:quickload "xmls")                   ;TODO: add quicklisp as a dependency
 
-(defun analyze (input-file)
+(defun to-sexpr (input-file)
   (with-open-file (xml-str input-file :direction :input)
     (let ((xml-sexpr (xmls:parse xml-str)))
-      (princ xml-sexpr)
-      (princ #\newline))))
+      (cddr xml-sexpr)
+      )))
