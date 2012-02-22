f30479a1a460c6308cef732c369c40ed352e1544
Mike Heise <kaesees@gmail.com>
2012-02-22 07:26:48 -0500

    Cleanup and added a count-rules function (our first real analysis! :)
    + Put functions in our own namespace instead of polluting the global one
    + Added docstring to to-sexpr
    + Added count-rules

--- analyzer.lisp
+++ analyzer.lisp
@@ -1,7 +1,17 @@
-(ql:quickload "xmls")                   ;TODO: add quicklisp as a dependency
+(ql:quickload "xmls")                   ;TODO: document quicklisp as a dependency
+
+(defpackage :unmake
+  (:use :common-lisp))
+(in-package :unmake)
 
 (defun to-sexpr (input-file)
+  "Parse a file in the Common Build Format and return an sexpr representing
+  the dependency tree contained therein."
   (with-open-file (xml-str input-file :direction :input)
     (let ((xml-sexpr (xmls:parse xml-str)))
       (cddr xml-sexpr)
       )))
+
+(defun count-rules (xml-sexpr)
+  (if (eq xml-sexpr nil) 0
+    (+ 1 (count-rules (cdr xml-sexpr)))))
