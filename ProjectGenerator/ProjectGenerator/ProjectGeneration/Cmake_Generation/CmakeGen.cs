ee5f9bee1936999d2404e3dea470490e6bcfab5c
kotarf <kotarf@rpi.edu>
2012-03-02 01:34:09 -0500

    adding cmake generation classes

---
+++ ProjectGenerator/ProjectGenerator/ProjectGeneration/Cmake_Generation/CmakeGen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public delegate double Delegate_Prod(int a, int b);
+namespace ProjectGenerator.ProjectGeneration.Cmake_Generation
+{
+    class CmakeGen
+    {
+        public void CreateCmakeList(string fullyQualifiedPath, string unqualifiedName, Graph<BuildElement> buildsys)
+        {
+            TextWriter txtwriter = new TextWriter();
+            foreach(BuildElement elem in buildsys)
+            {
+
+
+            }
+        }
+    }
+}
